7e4ab883b32df8689309420ed5e481dbfbdcb569
Vibha Thumar <savaliya.vibha@gmail.com>
2022-05-30 14:29:34 +0200

    Exception Handler and Representation

---
+++ src/main/java/com/yourname/SpringBoot/exception/handler/ExceptionRepresentation.java
@@ -0,0 +1,10 @@
+package com.yourname.SpringBoot.exception.handler;
+
+import lombok.Data;
+
+@Data
+public class ExceptionRepresentation {
+
+    private String message;
+    private String code;
+}
